12bdf1423602c052080d3b69186c64ec4e0ce3da
Rasanpreet Singh <Rasansaini@gmail.com>
2023-01-18 23:40:53 +1300

    Add project files.

---
+++ Hooks/Driver.cs
@@ -0,0 +1,24 @@
+﻿using Mars_Project.Utilities;
+using TechTalk.SpecFlow;
+
+namespace Mars_Project.Hooks
+{
+    [Binding]
+    public sealed class Driver : CommonDriver
+    { 
+
+        [BeforeScenario]
+        public void Setup()
+        {
+            //launch the browser
+            Initialize();
+
+        }
+
+        [AfterScenario]
+        public void TearDown()
+        {
+            CloseTestRun();
+        }
+    }
+}             
